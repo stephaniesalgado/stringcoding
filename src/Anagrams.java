1a46d733c88af004a1c8eb1d3d14ab205ea89c5c
Stephanie Salgado <stephaniecodes@icloud.com>
2024-06-06 09:14:35 -0700

    Java

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,18 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Anagrams {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         System.out.println(isAnagram("aab", "baa"));
         System.out.println(isAnagram("dog", "god"));
         System.out.println(isAnagram("hello", "goodbye"));
 
     }
 
-    private static boolean isAnagram(String str1, String str2){
+    private static boolean isAnagram(String str1, String str2) {
         int count[] = new int[256];
 
-        for(int i = 0; i < str1.length(); i++){
+        for(int i = 0; i < str1.length(); i++) {
             count[str1.charAt(i)]++;
             count[str2.charAt(i)]--;
 
---
+++ src/Balanced.java
@@ -0,0 +1,7 @@
+public class Balanced {
+
+    public static void main(String[] args) {
+
+    }
+
+}
